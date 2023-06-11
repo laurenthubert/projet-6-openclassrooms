ea6abfbe553cb6a15292640aad37fde4b0d9ae35
laurenthubert <laurenthubert59130@hotmail.com>
2023-06-11 20:17:24 +0200

    envoi sur github

--- Desktop/Print-it-JS-main/assets/script.js
+++ Desktop/Print-it-JS-main/assets/script.js
@@ -1,55 +1,65 @@
 ////////////////////liste des constantes
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-const bullets = document.querySelectorAll('.dot')
-const texteamodifier = banner.querySelector('p')
-const imagesvisible = document.getElementById('placeimagevisible')
-
-////////////////////place du premier bullet visible de bullets
-let bulletactif = 0 
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+const bullets = banner.querySelectorAll(".dot");
+const texteamodifier = document.querySelector("p");
+const imagesvisible = document.getElementById("placeimagevisible");
+////////////////////place du premier bullet
+let bulletactif = 0;
+////////////////////bullet visible
 function bulletselectionne() {
-	bullets.forEach((dot, bulletselectionne) => {
-		dot.classList.remove('dot_selected')
-		if (bulletselectionne === bulletactif) {
-			dot.classList.add('dot_selected')
-		}
-	})
+  bullets.forEach((dot, bulletselectionne) => {
+    dot.classList.remove("dot_selected");
+    if (bulletselectionne === bulletactif) {
+      dot.classList.add("dot_selected");
+    }
+  });
 }
-
-////////////////////deplacement des bullets ,des images et changement de texte
-
-
+/////////////////////image visible
+function imageselectionnee() {
+  imagesvisible.src = "./assets/images/slideshow/" + slides[bulletactif].image;
+}
+////////////////////texte visible
+function texteselectionne() {
+  texteamodifier.innerHTML = slides[bulletactif].tagLine;
+}
+////////////////////regle du click , deplacement des bullets ,des images et du changement de texte
 function precedente() {
-	bulletactif = (bulletactif - 1 + slides.length) % slides.length
-	imagesvisible.src = './assets/images/slideshow/' + slides[bulletactif].image
-	texteamodifier.innerHTML = slides[bulletactif].tagLine
-	bulletselectionne()
+  bulletactif = bulletactif - 1;
+  if (bulletactif > 3) bulletactif = 0;
+  if (bulletactif < 0) bulletactif = 3;
+  bulletselectionne();
+  imageselectionnee();
+  texteselectionne();
 }
 function suivante() {
-	bulletactif = (bulletactif + 1) % slides.length
-	imagesvisible.src = './assets/images/slideshow/' + slides[bulletactif].image
-	texteamodifier.innerHTML = slides[bulletactif].tagLine
-	bulletselectionne()
-
+  bulletactif = bulletactif + 1;
+  if (bulletactif > 3) bulletactif = 0;
+  if (bulletactif < 0) bulletactif = 3;
+  bulletselectionne();
+  imageselectionnee();
+  texteselectionne();
 }
-
 /////////////////////declenchement des evenements
-
-document.querySelector('.arrow_left').addEventListener('click', precedente)
-document.querySelector('.arrow_right').addEventListener('click', suivante)
+document.querySelector(".arrow_left").addEventListener("click", precedente);
+document.querySelector(".arrow_right").addEventListener("click", suivante);
+////////////////////ajout personnel/////////////////////////////////////////////////////////
+////////////////////declanchement automatique
+setInterval("suivante(1)", 5000);
